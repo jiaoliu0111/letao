9f8e04c8e4919f3308b63d35241266ca199dbb77
jiaoliu0111 <2805115506@qq.com>
2018-11-01 16:02:45 +0800

    完成user的分页功能

--- js/user.js
+++ js/user.js
@@ -1,19 +1,53 @@
 $(function(){
-  $.ajax({
-    type:'get',
-    url:'/user/queryUser',
-    data:{
-      page:1,
-      pageSize:5
-    },
-    dataType:'json',
-    success:function(info){
-      console.log(info);
-      var htmlStr = template('tmp',info);
-      // console.log(htmlStr);
-      $('tbody').html(htmlStr);
-      
-    }
 
-  })
+  var currentPage = 1;
+  var pageSize = 5;
+  // 一进入页码，就渲染一次
+  render();
+
+
+  function render(){
+    $.ajax({
+      type:'get',
+      url:'/user/queryUser',
+      data:{
+        page:currentPage,
+        pageSize:pageSize
+      },
+      dataType:'json',
+      success:function(info){
+        console.log(info);
+        var htmlStr = template('tmp',info);
+        // console.log(htmlStr);
+        $('tbody').html(htmlStr);
+        
+        // 请求到后台数据后，配置分页
+        $('#paginator').bootstrapPaginator({
+          bootstrapMajorVersion:3,
+          currentPage:info.page,
+          totalPages:Math.ceil(info.total/info.size),
+          // 页码点击事件
+          onPageClicked:function(a,b,c,page){
+            //console.log(page);      //点第几页，就输出几
+            currentPage = page;
+            render();
+          }
+        })
+      }
+
+    })
+  }
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
 })
